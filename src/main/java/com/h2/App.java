82e0b996d5d8ef28ef15fed7cb0a4a032b43d378
jonnyh2401 <63194091+jonnyh2401@users.noreply.github.com>
2022-11-23 12:43:43 +0000

    Completed Loan Rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,24 @@
 package com.h2;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+
+        return number * 2;
     }
+
+    private static int add(int[] numbers) {
+        int sum = 1;
+
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+
+        return sum;
+    }
+
 }
